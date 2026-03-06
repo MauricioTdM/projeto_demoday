e9aa638572ce8b39619fe400e7dc2b66b48b74da
MauricioTdM <mauricio1tmelo@yahoo.com.br>
2026-03-05 22:41:45 -0300

    teste de erro

--- frontend/src/Pages/QueroDoar/queroDoar.jsx
+++ frontend/src/Pages/QueroDoar/queroDoar.jsx
@@ -10,7 +10,7 @@ export default function QueroDoar() {
     const [autor, setAutor] = useState("")
     const [imagem_url, setImagem_url] = useState("")
 
-    console.log(variavelQueNaoExiste)
+    console.log(variavelQueNaoExiste);
 
     const enviarDados = async() => {
         const urlApi = `${import.meta.env.VITE_API_URL}/doar`
